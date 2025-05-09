c75cd830dc02e278d3454acf5b5834b6b37ccc64
Ylenia <yleniamighela@gmail.com>
2025-05-09 10:56:06 +0200

    snack 01

--- src/useSwitch.jsx
+++ src/useSwitch.jsx
@@ -1,3 +1,14 @@
-export default function useSwitch() {
 
+import { useState, useCallback } from "react";
+
+export default function useSwitch(initialValue = false) {
+
+    const [isOn, setIsOn] = useState(initialValue);
+
+    // useCallback memorizza la funzione `toggle`, così da non ricrearla ad ogni render 
+    const toggle = useCallback(() => {
+        setIsOn(prev => !prev);
+    }, []);
+
+    return [isOn, toggle];
 }
